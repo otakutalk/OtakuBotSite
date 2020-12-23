0f89faecc6d68339b44644062fae8f486d183461
GothamGamer117 <43589019+GothamGamer117@users.noreply.github.com>
2020-12-23 14:08:45 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,25 @@
-function () {
+function handle(e){
+        if(e.keyCode === 13){
+            e.preventDefault(); // Ensure it is only this code that runs
+			mainCheck();
+        }
+    }
 
+function mainCheck() {
+	var password = 'crtmh';
+	var passBox = document.getElementById('passBox');
+	var input = document.getElementById('passBox').value;
+	//var input = prompt("Please enter the password to get back to the main page: ","");
+	
+	if(input === null) {
+		alert('Please enter a password to continue');
+	}else	if(input != password) {
+		alert('Incorrect password!');
+		passBox.value = '';
+		passBox.focus();
+	}else {
+	location.href='./mainPage.html';
+	//localStorage.setItem('passCheck', null);
+	sessionStorage.setItem("passCheck",1);
+	}
 }
