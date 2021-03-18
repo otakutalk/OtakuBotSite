020b560f5498aa9785a4070266a8eb27c5ecf335
GothamGamer117 <43589019+GothamGamer117@users.noreply.github.com>
2021-03-18 17:05:04 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -7,19 +7,23 @@ function handle(e){
 
 function mainCheck() {
 	var password = 'crtmh';
+	var testPass = 'uwu';
 	var passBox = document.getElementById('passBox');
 	var input = document.getElementById('passBox').value;
 	//var input = prompt("Please enter the password to get back to the main page: ","");
 	
 	if(input === null) {
 		alert('Please enter a password to continue');
-	}else	if(input != password) {
-		alert('Incorrect password!');
-		passBox.value = '';
-		passBox.focus();
-	}else {
+	}else if(input == password){
 	location.href='./mainPage.html';
 	//localStorage.setItem('passCheck', null);
 	sessionStorage.setItem("passCheck",1);
+	}else if(input == testPass){
+		location.href='./html/testPage.html';
+		sessionStorage.setItem('testCheck',1);
+	}else	if(input != password && input!= testPass) {
+		alert('Incorrect password!');
+		passBox.value = '';
+		passBox.focus();
 	}
 }
